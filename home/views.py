e5a1e016a16961da721a33faf580e8ff64e41b64
hmzugur <craxx67@gmail.com>
2021-03-31 22:23:58 +0300

    home app olsturuldu, içinde templates->index.html & text yazdık

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 def index(request):
 
-    text = "My text ..."
+    text = "My text ... <br>Ugur hmz <strong>Burası BOSTON/MA</strong>"
     context = {'text':text}
 
     return render(request,"index.html", context=context)
