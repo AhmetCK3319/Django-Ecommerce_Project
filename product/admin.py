d046bf5c49b3ab5d1bf6ae50eb3540f61f81b5d9
hmzugur <craxx67@gmail.com>
2021-04-01 09:32:21 +0300

    2 tane Product eklendi, admin panelinde listelendi

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-from product.models import Category
+from product.models import Category,Product
 
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+        search_fields = ['title', 'description', 'keywords']
         list_display=['title','status']
         list_filter=['status','create_at']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+        list_display = ['title','category','price','amount','status']
+        list_filter = ['status','create_at','category']
+        search_fields = ['title','category','price','keywords']
--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 
-
+#_________________________________ Category(models.Model)_________________________________________
 class Category(models.Model):
 
     STATUS = (
@@ -23,3 +23,53 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+#_________________________________ Product(models.Model)_________________________________________
+
+class Product(models.Model):
+
+    STATUS = (
+        ('True','Evet'),
+        ('False','Hayır'),
+    )
+    category= models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length = 10,choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
