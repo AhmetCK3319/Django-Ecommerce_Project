d477cd98db86224bb75bd3143863eb8068e6df7d
hmzugur <craxx67@gmail.com>
2021-04-04 20:21:55 +0300

    url düzeltildi..

--- project_conf/urls.py
+++ project_conf/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('hakkimizda/',views.hakkimizda, name='hakkimizda'),
     path('referanslar/',views.referanslar, name='referanslar'),
     path('iletisim/',views.iletisim, name='iletisim'),
-    path('home/',include('home.urls')),
+
     path('product/', include('product.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_products,name='category_products'),
